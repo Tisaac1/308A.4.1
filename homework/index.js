5eadce462441f9447cfd06a413aedfb7402d8af8
Tisaac1 <lashay_isaac@yahoo.com>
2024-07-21 02:19:44 -0400

    update

--- homework/index.js
+++ homework/index.js
@@ -1,1 +1,5 @@
-alert('3')
+let generate_btbn = document.querySelector('.generate_btn')
+let animal_type = document.querySelector("#cat_breed")
+let num_facts = document.querySelector("#num_facts")
+
+generate_btbn.addEventListener()
